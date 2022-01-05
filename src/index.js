874c80d08cd857162aa7de5c6ff0a10a464f2a33
[Valentina] <[mariaiscus1@gmail.com]>
2022-01-05 13:39:06 +0600

    fix: fix task solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,3 @@
 module.exports = function reverse(n) {
-  return +(Math.abs(n) + '').split('').reduce((acc, elem) => elem + acc);
+  return +((Math.abs(n) + '').split('').reduce((acc, elem) => elem + acc));
 }
